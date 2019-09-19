3c43a16e7c1ddb7beed490b393833cc58f03a809
Student <student@kr.step.ua>
2019-09-19 20:42:59 +0300

    add hotels

--- pages/admin.php
+++ pages/admin.php
@@ -1,4 +1,5 @@
 <?php 
+    // A
     if(isset($_POST['add_country'])){
         $country = trim(htmlspecialchars($_POST['country']));
         if($country == ''){
@@ -17,7 +18,7 @@
         }
         $msg = "<span class='alert alert-success'>Country deleted</span>";
     }
-
+    // B
     if(isset($_POST['add_city'])){
         $city = trim(htmlspecialchars($_POST['city']));
         $country_id = $_POST['country_id'];
@@ -36,6 +37,34 @@
             mysqli_query(connect(), $del);
         }
         $msg_city = "<span class='alert alert-success'>City deleted</span>";
+    }
+    //C
+    if(isset($_POST['add_hotel'])){
+        $hotel = trim(htmlspecialchars($_POST['hotel']));
+        $_city_country_id = explode(':',$_POST['city_country_id']);
+        //echo print_r($_city_country_id);
+        $_city_id = $_city_country_id[0];
+        $_country_id =$_city_country_id[1];
+        //echo $_city_id.'<br>'.$_country_id.'<br';
+        $stars = $_POST['stars'];
+        $price = $_POST['price'];
+        $info = $_POST['info'];
+        //echo $stars.'<br>'.$price.'<br>'.$info;
+        if($hotel === '' || $stars === '' || $price === '' || $info === ''){
+            echo "<script>window.location='index.php?page=4'</script>";
+            return;
+        }
+        $_ins = "insert into hotels(hotel, country_id, city_id, stars, price, info) values('$hotel', $_country_id, $_city_id, $stars, '$price', '$info')";
+        $link = connect();
+        mysqli_query($link, $_ins);
+        $msg_hotel = "<span class='alert alert-success'>Hotel added</span>";
+    }
+    if(isset($_POST['del_hotel'])){
+        foreach($_POST['indexes'] as $id){
+            $del = "delete from hotels where id=$id";
+            mysqli_query(connect(), $del);
+        }
+        $msg_city = "<span class='alert alert-success'>Hotel deleted</span>";
     }
 ?>
 <h1>Admin</h1>
@@ -156,12 +185,83 @@
             <h3>Form Hotels</h3>
         </div>
         <div class="panel-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum voluptas laudantium eligendi accusantium officia maxime enim delectus inventore. Cumque saepe vero architecto soluta vel? Aperiam, deserunt nulla dolor velit rerum maiores officia dolore nam enim reiciendis reprehenderit. Provident, reprehenderit quibusdam?
-        </div>
-        <div class="panel-footer">
-            <button class="btn btn-warning">Add</button>
-            <button class="btn btn-primary">Delete</button>
-        </div>
+        <form action="index.php?page=4" method="post">
+            <div class="panel-heading">
+                <h3>Form Hotels</h3>
+            </div>
+            <div class="panel-body">
+                <?php
+                    $link = connect();
+                    $sel_hotel = 'select hotels.id, hotels.city_id, hotels.country_id, hotels.stars, hotels.price, hotels.hotel, cities.city, cities.id, countries.id, countries.country from hotels, cities, countries where cities.country_id = countries.id and hotels.city_id = cities.id and hotels.country_id = countries.id';
+                    $sel_country_city = "select cities.id, cities.city, countries.country, countries.id from cities, countries where cities.country_id = countries.id";
+                    $hotel_res = mysqli_query($link, $sel_hotel);
+                    $city_country_res = mysqli_query($link, $sel_country_city);
+                ?>
+                <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>hotel</th>
+                        <th>stars</th>
+                        <th>price</th>
+                        <th>city/country</th>
+                        <th>options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = mysqli_fetch_array($hotel_res)):?>
+                    <tr>
+                        <td><?=$row[0]?></td>
+                        <td><?=$row[5]?></td> 
+                        <td><?=$row[3]?></td> 
+                        <td><?=$row[4]?></td>  
+                        <td><?=$row[6].'/'.$row[9]?></td> 
+                        <td>
+                            <input type="checkbox" name="indexes[]" value="<?=$row[0]?>">
+                        </td>
+                    </tr>
+                    <? endwhile;?>
+                </tbody>
+                </table>
+                <div class="form-group">
+                    <select name="city_country_id" class="form-control">
+                        <?php while ($row = mysqli_fetch_array($city_country_res )):?>
+                        <option value="<?=$row[0].':'.$row[3]?>"><?=$row[1].' / '.$row[2]?></option>
+                        <? endwhile;?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>
+                        Hotel Title
+                        <input class="form-control" type="text" name="hotel">
+                    </label>    
+                </div>
+                <div class="form-group">
+                    <label>
+                        Price
+                        <input class="form-control" type="text" name="price">
+                    </label>    
+                </div>
+                <div class="form-group">
+                    <label>
+                        Stars
+                        <input class="form-control" type="number" name="stars" min="1" max="5">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>
+                        Info
+                        <textarea class="form-control" name="info" cols="22" rows="6"></textarea>
+                    </label>
+                </div>
+            </div>
+            <div class="panel-footer">
+                <button class="btn btn-primary" name="add_hotel" type="submit">Add</button>
+                <button class="btn btn-danger" name="del_hotel" type="submit">Delete</button>
+                <?= $msg_hotel?:''?>
+            </div>
+            </form>            
+       </div>
     </div>
 </div>
 <div class="col-md-6">
